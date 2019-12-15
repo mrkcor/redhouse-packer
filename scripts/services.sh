27c48ae219a6ebfd5fc626b6ad7c22e88b0279f0
Mark Cornelissen <cornelissen@srs.nl>
2019-12-15 11:28:09 +0100

    Various fixes
    + MariaDB 10.4 from MariaDB repo
    + Nginx requires the x11 libraries

--- scripts/services.sh
+++ scripts/services.sh
@@ -5,26 +5,21 @@ export DEBIAN_FRONTEND=noninteractive
 # NodeJS
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 apt-get install -y nodejs
+/usr/bin/npm install -g npm
+/usr/bin/npm install -g yarn
 
-# Yarn
-curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-echo \"deb https://dl.yarnpkg.com/debian/ stable main\" | sudo tee /etc/apt/sources.list.d/yarn.list
-sudo apt-get update && sudo apt-get install yarn
+# MariaDB
+sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xF1656F24C74CD1D8
+sudo add-apt-repository 'deb [arch=amd64,arm64,ppc64el] http://ftp.ubuntu-tw.org/mirror/mariadb/repo/10.4/ubuntu bionic main'
 
-# MariaDB -- TODO: get mariadb from the official mariadb repository
-apt-get -y intall mariadb-server
+apt-get -y update
+apt-get -y install mariadb-server
 
-# Configure MySQL Password Lifetime
-echo "default_password_lifetime = 0" >> /etc/mysql/mysql.conf.d/mysqld.cnf
-
-# Configure MySQL Remote Access
-sed -i '/^bind-address/s/bind-address.*=.*/bind-address = 0.0.0.0/' /etc/mysql/mysql.conf.d/mysqld.cnf
-
-mysql --user="root" --password="secret" -e "GRANT ALL ON *.* TO root@'0.0.0.0' IDENTIFIED BY 'overyonder' WITH GRANT OPTION;"
+# Configure MySQL Access
+mysql --user="root" --password="secret" -e "GRANT ALL ON *.* TO root@'%' IDENTIFIED BY 'overyonder' WITH GRANT OPTION;"
 service mysql restart
 
-mysql --user="root" --password="secret" -e "CREATE USER 'redhouse'@'0.0.0.0' IDENTIFIED BY 'overyonder';"
-mysql --user="root" --password="secret" -e "GRANT ALL ON *.* TO 'redhouse'@'0.0.0.0' IDENTIFIED BY 'overyonder' WITH GRANT OPTION;"
+mysql --user="root" --password="secret" -e "CREATE USER 'redhouse'@'%' IDENTIFIED BY 'overyonder';"
 mysql --user="root" --password="secret" -e "GRANT ALL ON *.* TO 'redhouse'@'%' IDENTIFIED BY 'overyonder' WITH GRANT OPTION;"
 mysql --user="root" --password="secret" -e "FLUSH PRIVILEGES;"
 mysql --user="root" --password="secret" -e "CREATE DATABASE redhouse character set UTF8mb4 collate utf8mb4_bin;"
@@ -45,7 +40,7 @@ sudo -u postgres psql -c "CREATE ROLE redhouse LOGIN PASSWORD 'overyonder' SUPER
 sudo -u postgres /usr/bin/createdb --echo --owner=redhouse redhouse
 service postgresql restart
 
-# Others
+# Other services & tools
 apt-get -y install nginx sqlite3 libsqlite3-dev redis
 
 # Final package update
