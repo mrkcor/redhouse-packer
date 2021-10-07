982a018799ed82c64bab9bb0fe6ecbb38363f119
Mark Cornelissen <mark@boxture.com>
2021-10-07 05:54:06 +0200

    Update to PostgreSQL 14

--- scripts/services.sh
+++ scripts/services.sh
@@ -13,13 +13,13 @@ echo 'deb [arch=amd64] http://apt.postgresql.org/pub/repos/apt/ focal-pgdg main'
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
 apt-get update
 
-apt-get install -y postgresql-13 postgresql-server-dev-13 postgresql-13-postgis-3 postgresql-13-repack
+apt-get install -y postgresql-14 postgresql-server-dev-14 postgresql-14-postgis-3 postgresql-14-repack
 
 # Configure Postgres Remote Access
-sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/g" /etc/postgresql/13/main/postgresql.conf
-sed -i "s/local   all             all                                     peer/local   all             all                                     trust/g" /etc/postgresql/13/main/pg_hba.conf
-sed -i "s/host    all             all             127.0.0.1\/32            md5/host    all             all             127.0.0.1\/32            trust/g" /etc/postgresql/13/main/pg_hba.conf
-sed -i "s/host    all             all             ::1\/128                 md5/host    all             all             ::1\/128                 trust/g" /etc/postgresql/13/main/pg_hba.conf
+sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/g" /etc/postgresql/14/main/postgresql.conf
+sed -i "s/local   all             all                                     peer/local   all             all                                     trust/g" /etc/postgresql/14/main/pg_hba.conf
+sed -i "s/host    all             all             127.0.0.1\/32            md5/host    all             all             127.0.0.1\/32            trust/g" /etc/postgresql/14/main/pg_hba.conf
+sed -i "s/host    all             all             ::1\/128                 md5/host    all             all             ::1\/128                 trust/g" /etc/postgresql/14/main/pg_hba.conf
 sudo -u postgres psql -c "CREATE ROLE vagrant LOGIN SUPERUSER INHERIT NOCREATEDB NOCREATEROLE NOREPLICATION;"
 sudo -u postgres psql -c "CREATE ROLE root LOGIN SUPERUSER INHERIT NOCREATEDB NOCREATEROLE NOREPLICATION;"
 service postgresql restart
