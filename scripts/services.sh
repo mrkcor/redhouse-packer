a79c33a7a87bd8f58c67b47df01958c8d4fca7a1
Mark Cornelissen <mark@boxture.com>
2021-10-07 06:17:55 +0200

    Update postgresql 14 config updates

--- scripts/services.sh
+++ scripts/services.sh
@@ -18,8 +18,8 @@ apt-get install -y postgresql-14 postgresql-server-dev-14 postgresql-14-postgis-
 # Configure Postgres Remote Access
 sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/g" /etc/postgresql/14/main/postgresql.conf
 sed -i "s/local   all             all                                     peer/local   all             all                                     trust/g" /etc/postgresql/14/main/pg_hba.conf
-sed -i "s/host    all             all             127.0.0.1\/32            md5/host    all             all             127.0.0.1\/32            trust/g" /etc/postgresql/14/main/pg_hba.conf
-sed -i "s/host    all             all             ::1\/128                 md5/host    all             all             ::1\/128                 trust/g" /etc/postgresql/14/main/pg_hba.conf
+sed -i "s/host    all             all             127.0.0.1\/32            scram-sha-256/host    all             all             127.0.0.1\/32            trust/g" /etc/postgresql/14/main/pg_hba.conf
+sed -i "s/host    all             all             ::1\/128                 scram-sha-256/host    all             all             ::1\/128                 trust/g" /etc/postgresql/14/main/pg_hba.conf
 sudo -u postgres psql -c "CREATE ROLE vagrant LOGIN SUPERUSER INHERIT NOCREATEDB NOCREATEROLE NOREPLICATION;"
 sudo -u postgres psql -c "CREATE ROLE root LOGIN SUPERUSER INHERIT NOCREATEDB NOCREATEROLE NOREPLICATION;"
 service postgresql restart
