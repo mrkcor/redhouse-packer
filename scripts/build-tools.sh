7c765aa42e8987e66d59a2fe481cdae364cbb09c
Mark Cornelissen <mark@boxture.com>
2022-01-31 13:21:06 +0100

    Adds git-flow

--- scripts/build-tools.sh
+++ scripts/build-tools.sh
@@ -2,4 +2,4 @@
 
 export DEBIAN_FRONTEND=noninteractive
 
-apt-get -y install build-essential bison autoconf libtool-bin libxml2-dev libxslt1-dev libcurl4-openssl-dev cmake pkg-config
+apt-get -y install build-essential bison autoconf libtool-bin libxml2-dev libxslt1-dev libcurl4-openssl-dev cmake pkg-config git-flow
